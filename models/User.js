ff357a50e6011880cee656317c7ee19e61114836
YellowFlash2012 <cyclop9@hotmail.com>
2022-07-11 06:21:37 +0100

    created the userschema, hashed the pw for the signup route, compared the password for the login route and created the jwt for login

---
+++ models/User.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcryptjs";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken"
+import validator from "validator";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: [true, "Please enter your full name"] },
+        email: {
+            type: String,
+            required: [true, "Please enter a valid email address"],
+            validate: {
+                validator: validator.isEmail,
+                message: "Please provide a valid email address",
+            },
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            validate: {
+                validator: validator.isStrongPassword,
+                message: "Your password must have at least 1 special character",
+            },
+            minlength: 13,
+            maxlength: 21,
+        },
+        isAdmin: { type: Boolean, required: true, default: false },
+    },
+    { timestamps: true }
+);
+
+// ***comparing the pw for the login route
+userSchema.methods.matchPaswword = async function (enteredPw) {
+    return await bcrypt.compare(enteredPw, this.password);
+};
+
+// ***hashing the pw for the signup route
+userSchema.pre("save",async function (next) {
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
+})
+
+// ***creating the jwt token
+userSchema.methods.createJWT = function () {
+    return jwt.sign({ id: this._id }, process.env.jwt_secret, { expiresIn: "9d" });
+};
+
+const User = mongoose.model("user", userSchema);
+
+export default User
--- server.js
+++ server.js
@@ -4,10 +4,17 @@ import { config } from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
 
+import connectDB from "./config/db.js";
+
+
 config()
 const app = express();
 app.use(express.json())
 app.use(helmet())
+
+app.get("/", (req, res) => {
+    res.send("We are live and running!");
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +22,8 @@ if (process.env.NODE_ENV === "development") {
     app.use(morgan('dev'))
 }
 
+
+connectDB()
 app.listen(PORT, () => {
     console.log(
         `Server running in ${process.env.NODE_ENV} mode | Port ${PORT}`.yellow.bold
